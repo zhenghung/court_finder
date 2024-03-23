a980d587a92af1dfae295c64353142f2ba59840c
Zayne Chuah <zheng_hung@hotmail.com>
2024-03-23 15:53:47 +0000

    Added Flask App

---
+++ flask_app.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, jsonify, send_file, send_from_directory, render_template
+import os
+import time
+import concurrent.futures
+
+# Import functions from your script
+from prototype_script import get_venues, get_activities_for_date, process_venue, print_times_info
+
+app = Flask(__name__)
+
+# Define the directory path where the index.html file is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+@app.route('/process_venues', methods=['GET'])
+def process_venues():
+    date = request.args.get('date')
+    if not date:
+        return jsonify({'error': 'Date parameter is required'}), 400
+
+    start = time.time()
+
+    # Fetch list of venues
+    venues_data = get_venues()
+
+    # Filter venues by area == 'London'
+    london_venues = [venue for venue in venues_data if venue.get('area', '') == 'London']
+    london_venues_with_no_badminton_ids = [90121, 90127, 90135, 90141, 90014, 90143, 90019, 90147, 90277, 90278, 90023, 90024, 90281, 90280, 90155, 90282, 90157, 90030, 90031, 90159, 90160, 90161, 90291, 90162, 90037, 90294, 90289, 90165, 90297, 90168, 90171, 90173, 90175, 90049, 90178, 90179, 90054, 90056, 90185, 90189, 90061, 90081, 90217, 90218, 90219, 90221, 90222, 90223, 90224, 90102, 90107]
+    london_badminton_venues = [venue for venue in london_venues if venue.get('id', '') not in london_venues_with_no_badminton_ids]
+
+    # Check if there are London venues
+    if london_badminton_venues:
+        # Process venues using ThreadPoolExecutor with a fixed number of threads (8 threads)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            results = list(executor.map(process_venue, london_badminton_venues, [date]*len(london_badminton_venues)))
+
+        available_times_found = [result for result in results if 'available_times' in result]
+        venues_with_no_times = {result['venue'].get('slug', '') for result in results if 'no_times' in result}
+
+        print("\n=== Completed search ===\n")
+        end = time.time()
+        print("\nTime elapsed")
+        print(end - start)
+
+        # Construct the response with venues and their available times
+        response_data = []
+        for result in available_times_found:
+            venue = result['venue']
+            venue_id = venue.get('id', '')
+            venue_name = venue.get('name', '')
+            venue_town = venue.get('town', '')
+            venue_area = venue.get('area', '')
+            venue_postcode = venue.get('postcode', '')
+
+            # Extract available times for the venue
+            available_times = result['available_times']
+            available_times_info = []
+            for time_info in available_times:
+                starts_at_12h = time_info.get('starts_at', {}).get('format_12_hour', '')
+                ends_at_12h = time_info.get('ends_at', {}).get('format_12_hour', '')
+                duration = time_info.get('duration', '')
+                price = time_info.get('price', {}).get('formatted_amount', '')
+                location = time_info.get('location', '')
+                spaces = time_info.get('spaces', 0)
+                name = time_info.get('name', '')
+                available_times_info.append({
+                    'starts_at_12h': starts_at_12h,
+                    'ends_at_12h': ends_at_12h,
+                    'duration': duration,
+                    'price': price,
+                    'location': location,
+                    'spaces': spaces,
+                    'name': name
+                })
+
+            # Append venue info with available times to the response data
+            response_data.append({
+                'venue_name': venue_name,
+                'venue_id': venue_id,
+                'venue_location': f"{venue_town}, {venue_area} - {venue_postcode}",
+                'available_times': available_times_info
+            })
+
+        return jsonify({'venues_with_times': response_data}), 200
+
+    else:
+        return jsonify({'message': 'No venues found in the response.'}), 404
+
+@app.route('/')
+def index():
+    return send_file(os.path.join(STATIC_DIR, 'index.html')) 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ prototype_script.py
@@ -0,0 +1,139 @@
+import requests
+import time
+import concurrent.futures
+
+def get_venues():
+    base_url_venues = 'https://better-admin.org.uk/api/activities/venues'
+    headers = {
+        'accept': 'application/json',
+        'origin': 'https://bookings.better.org.uk',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    }
+    response_venues = requests.get(base_url_venues, headers=headers)
+    data = response_venues.json().get('data', [])
+    return data
+
+def get_activities_for_date(venue_id, category_slug, date):
+    base_url_times = f'https://better-admin.org.uk/api/activities/venue/{venue_id}/activity/{category_slug}/times'
+    headers = {
+        'accept': 'application/json',
+        'origin': 'https://bookings.better.org.uk',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    }
+    params = {
+        'date': date
+    }
+    response_times = requests.get(base_url_times, headers=headers, params=params)
+    data = response_times.json().get('data', [])
+    if type(data) is not list:
+        return [data]
+    return data
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+def print_times_info(times):
+    for time in times:
+        starts_at_12h = time.get('starts_at', {}).get('format_12_hour', '')
+        ends_at_12h = time.get('ends_at', {}).get('format_12_hour', '')
+        duration = time.get('duration', '')
+        price = time.get('price', {}).get('formatted_amount', '')
+        location = time.get('location', '')
+        spaces = time.get('spaces', 0)
+        name = time.get('name', '')
+
+        print(f"\nTime: {starts_at_12h} - {ends_at_12h}")
+        print(f"Duration: {duration}")
+        print(f"Price: {price}")
+        print(f"Location: {location}")
+        print(f"Spaces available: {spaces}")
+        print(f"Activity: {name}")
+
+def process_venue(venue, date):
+    venue_id = venue.get('id', '')
+    venue_name = venue.get('name', '')
+    venue_town = venue.get('town', '')
+    venue_area = venue.get('area', '')
+    venue_postcode = venue.get('postcode', '')
+
+    # Fetch activities for the specified date and venue
+    times = get_activities_for_date(venue_id, 'badminton-40min', date) + get_activities_for_date(venue_id, 'badminton-60min', date)
+
+    available_times = [time for time in times if time.get('spaces', 0) > 0]
+    if available_times:
+        return {'venue': venue, 'available_times': available_times}
+    else:
+        return {'venue': venue, 'no_times': True}
+
+def main():
+    start = time.time()
+
+    # Fetch list of venues
+    venues_data = get_venues()
+
+    # Set the date variable
+    date = '2024-03-17'
+
+    # Filter venues by area == 'London'
+    london_venues = [venue for venue in venues_data if venue.get('area', '') == 'London']
+    london_venues_with_no_badminton_ids = [90121, 90127, 90135, 90141, 90014, 90143, 90019, 90147, 90277, 90278, 90023, 90024, 90281, 90280, 90155, 90282, 90157, 90030, 90031, 90159, 90160, 90161, 90291, 90162, 90037, 90294, 90289, 90165, 90297, 90168, 90171, 90173, 90175, 90049, 90178, 90179, 90054, 90056, 90185, 90189, 90061, 90081, 90217, 90218, 90219, 90221, 90222, 90223, 90224, 90102, 90107]
+    london_badminton_venues = [venue for venue in london_venues if venue.get('id', '') not in london_venues_with_no_badminton_ids]
+
+    # Check if there are London venues
+    if london_badminton_venues:
+        # Process venues using ThreadPoolExecutor with a fixed number of threads (8 threads)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            results = list(executor.map(process_venue, london_badminton_venues, [date]*len(london_badminton_venues)))
+
+        available_times_found = [result for result in results if 'available_times' in result]
+        venues_with_no_times = {result['venue'].get('slug', '') for result in results if 'no_times' in result}
+
+        print("\n=== Completed search ===\n")
+        end = time.time()
+        print("\nTime elapsed")
+        print(end - start)
+
+        # Printing the times
+        print(f'\n{len(available_times_found)} venues with times found')
+        for result in available_times_found:
+            venue = result['venue']
+            venue_id = venue.get('id', '')
+            venue_name = venue.get('name', '')
+            venue_town = venue.get('town', '')
+            venue_area = venue.get('area', '')
+            venue_postcode = venue.get('postcode', '')
+            print(f"\n\nVenue: {venue_name}")
+            print(f"ID: {venue_id}")
+            print(f"Location: {venue_town}, {venue_area} - {venue_postcode}")
+            print_times_info(result['available_times'])
+
+        print(f'\n {len(venues_with_no_times)} venues with no times')
+        print(venues_with_no_times)
+        print("\n=======")
+
+    else:
+        print("No venues found in the response.")
+
+if __name__ == "__main__":
+    main()
